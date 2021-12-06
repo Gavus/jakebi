47fc39d9d9973fe09bc59ac6372c32b155375d3b
Gustav Johansson <gustavsj@outlook.com>
2021-12-06 22:30:46 +0100

    Initial commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+export JEKYLL_VERSION=3.8
+mkdir -p _site
+podman run \
+    -v $PWD:/srv/jekyll:rw \
+    -v $PWD/_site:/srv/jekyll/_site:rw \
+    -e PAGES_REPO_NWO=$PWD \
+    -p 4000:4000 \
+    --rm \
+    --name jakebi \
+    docker.io/jekyll/builder:$JEKYLL_VERSION bash -c "chmod 777 /srv/jekyll && jekyll serve"
+
